ba88c49c46fd2b5ca1601fdd521f78157e98af90
Nakkiran Sunassee <n.sunassee@gmail.com>
2019-03-25 14:20:07 +0200

    Initial commit

---
+++ ui.R
@@ -0,0 +1,37 @@
+library(shiny) 
+
+shinyUI(
+  pageWithSidebar(
+    # Application title
+    headerPanel(" Body Mass Index (BMI) Calculator"),
+    
+    sidebarPanel(
+      p('Body Mass Index is an estimate of the body fat of a person, calculated using their height and weight'),
+      p('The formula is BMI = kg/m2, with the weight in kg and m2 is their height in metres squared.'),
+      p('A healthy BMI is between 18.5 to 24.9. Should your BMI be over 25, this may indicate that you are overweight.'),
+      p('While BMI applies to most adults 18-65 years, please note that this is indicative only. Consult your doctor for further information'),
+      
+      tags$div(
+        tags$ul(
+          tags$li('BMI < 18.5 : You are Underweight'),
+          tags$li('BMI [18.5-24.9] : Your weight is Healthy'),
+          tags$li('BMI [25-29.9] : You are Overweight'),
+          tags$li('BMI >=30 : Your weight indicates Obesity. You should consult your doctor urgently!')
+        )
+      )
+    ),
+    
+    mainPanel(
+      numericInput('weight', 'Enter your weight (in kg)', 0),
+      numericInput('height', 'Enter your height (in m)', 0, min = 1.00, max = 2.19, step = 0.01),
+      submitButton('Calculate BMI'),
+      
+      h3('Calculating your BMI:'), 
+      p('weight:'), verbatimTextOutput("inputweightvalue"),
+      p('height:'), verbatimTextOutput("inputheightvalue"),
+      h4('Your BMI is:'),
+      verbatimTextOutput("estimation"),
+      p('Results:'),strong(verbatimTextOutput("diagnostic"))
+    )
+  )   
+)
